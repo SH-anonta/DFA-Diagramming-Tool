ff130f99b9a41e481b549f32bf84d78496f63f3d
Anonta <SH-anonta@users.noreply.github.com>
2018-05-16 19:17:45 +0600

    Add new component:login (empty) Add new component:register (empty)
    Add routers for new components

---
+++ src/app/page-components/readme.md
@@ -0,0 +1,5 @@
+## Page components
+
+These components only hold static parts of the page and they dictate the layout of the page
+
+All functional parts are carried out by other components (from other feature modules).
--- src/app/root-router.module.ts
+++ src/app/root-router.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core'
 import {RouterModule} from '@angular/router';
 import {HomepageComponent} from './page-components/homepage/homepage.component';
 import {Error404Component} from './page-components/error404/error404.component';
+import {LoginComponent} from './page-components/login/login.component';
+import {RegisterComponent} from './page-components/register/register.component';
 
 
 let routes = [
   {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 
   {path: 'error-404', component: Error404Component},
   {path: '**', redirectTo: 'error-404'},
@@ -14,7 +18,9 @@ let routes = [
 @NgModule({
   declarations: [
     HomepageComponent,
-    Error404Component
+    Error404Component,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     RouterModule.forRoot(routes)
