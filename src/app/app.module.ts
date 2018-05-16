89158b6c0d0dc46b698fe4b490244c1de7d172ed
Anonta <SH-anonta@users.noreply.github.com>
2018-05-16 18:51:43 +0600

    Add header and footer components Add header and footer to app component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {RootRouterModule} from './root-router.module';
+import {HeaderComponent} from './page-components/header/header.component';
+import {FooterComponent} from './page-components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
