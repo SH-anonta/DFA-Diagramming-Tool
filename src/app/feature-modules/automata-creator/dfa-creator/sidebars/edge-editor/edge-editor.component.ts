e31af8878a6ef7e69427a1279049f7245ae0874f
Anonta <SH-anonta@users.noreply.github.com>
2018-06-16 16:36:49 +0600

    Add new components for dfa creator sidebar

---
+++ src/app/feature-modules/automata-creator/dfa-creator/sidebars/edge-editor/edge-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dfa-edge-editor',
+  templateUrl: './edge-editor.component.html',
+  styleUrls: ['./edge-editor.component.css']
+})
+export class EdgeEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/feature-modules/automata-creator/dfa-creator/sidebars/node-editor/node-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dfa-node-editor',
+  templateUrl: './node-editor.component.html',
+  styleUrls: ['./node-editor.component.css']
+})
+export class NodeEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
